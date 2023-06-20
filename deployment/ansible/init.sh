fc0e5b62a44a9bfe93e76d5bc106c1dcb4c94694
Ivo Christ <ivo.christ@aqarios.com>
2023-06-20 12:45:36 +0200

    docker playbook

--- deployment/ansible/init.sh
+++ deployment/ansible/init.sh
@@ -4,7 +4,7 @@
 sudo apt -y install ansible sshpass
 
 # Add all of your machines to ssh known hosts
-for i in `seq -w 01 10`; do sshpass -p <password> ssh <username>@<username>vm1$i.rtb-lab.pl -o StrictHostKeyChecking=no -C "/bin/true"; done
+for i in `seq -w 01 10`; do sshpass -f password ssh st103@st103vm1$i.rtb-lab.pl -o StrictHostKeyChecking=no -C "/bin/true"; done
 
 # define ansible hosts
 sudo cp hosts /etc/ansible/hosts
